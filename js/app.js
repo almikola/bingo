d139ceadf3ff313083d8137fba5d749d62f840a9
almikola <almikola@gmail.com>
2017-05-15 20:56:17 +0100

    start building grid using jquery

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var Bingo = Bingo || {};
+
+Bingo.gridBase = 7;
+Bingo.gridHeight = 7;
+Bingo.width = 350;
+Bingo.height = 350;
+
+Bingo.createGrid = function() {
+  var body = document.getElementsByTagName('main')[0];
+  var grid = document.createElement('ul');
+  body.appendChild(grid);
+  for (var i = 0; i < Bingo.gridBase*Bingo.gridHeight; i++) {
+    var tile = document.createElement('li');
+    grid.appendChild(tile);
+    tile.style.width = (Bingo.width/Bingo.gridBase) + 'px';
+    tile.style.height = (Bingo.height/Bingo.gridHeight) + 'px';
+    grid.appendChild(tile);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', Bingo.createGrid.bind(Bingo));
