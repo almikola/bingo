f1f6abceb5d3981fd266c82264ea02d410e644dc
almikola <almikola@gmail.com>
2017-06-19 22:46:27 +0100

    Adding onclick functions

--- js/app.js
+++ js/app.js
@@ -1,21 +1,11 @@
-var Bingo = Bingo || {};
+window.onload = function() {
+  var lis = $('li');
+  // for (var i = 0; i < lis.length; i++) {
+  //   lis[i].addEventListener('click', function() {
+  //     console.log('click');
+  //     document.getElementByClass('card').style.display = 'none';
+  //     // + this.id ;
+  //   });
+  // }
 
-Bingo.gridBase = 7;
-Bingo.gridHeight = 7;
-Bingo.width = 700;
-Bingo.height = 700;
-
-Bingo.createGrid = function() {
-  var body = document.getElementsByTagName('main')[0];
-  var grid = document.createElement('ul');
-  body.appendChild(grid);
-  for (var i = 0; i < Bingo.gridBase*Bingo.gridHeight; i++) {
-    var tile = document.createElement('li');
-    grid.appendChild(tile);
-    tile.style.width = (Bingo.width/Bingo.gridBase) + 'px';
-    tile.style.height = (Bingo.height/Bingo.gridHeight) + 'px';
-    grid.appendChild(tile);
-  }
 };
-
-document.addEventListener('DOMContentLoaded', Bingo.createGrid.bind(Bingo));
